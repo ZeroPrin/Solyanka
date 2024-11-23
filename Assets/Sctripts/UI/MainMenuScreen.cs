5b97e8d60e67b5ac6c397d4f0e4546dbf2a25b0b
Renat <painren@mail.ru>
2024-11-23 19:25:28 +0300

    Add Main menu panel

--- Assets/Sctripts/UI/MainMenuScreen.cs
+++ Assets/Sctripts/UI/MainMenuScreen.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScreen : ScreenUI
 {
+    [SerializeField] private Button _scene1;
 
+    public void Initialized()
+    {
+        _scene1.onClick.AddListener(LoadScene1);
+    }
+
+    private void LoadScene1()
+    {
+
+    }
 }
